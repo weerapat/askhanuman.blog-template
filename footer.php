a5dd4975e4888ed2ab320ed97f3231310803448c
weerapat <weerapatsu@gmail.com>
2014-11-17 17:54:26 +0700

    add footer askhanuman style

--- footer.php
+++ footer.php
@@ -1,5 +1,113 @@
-<div class="footer">
-<p>And this is the footer.</p>
-</div>
+
+
+<footer class="center-block">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-3 col-sm-6 col-xs-12">
+
+                <ul class="list-unstyled">
+
+                    <li style="display:none;" itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
+                        <a itemprop="url" href="/"><span itemprop="title">บ้าน</span></a>
+                    </li>
+
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/about-us">
+                            <span itemprop="title">เกี่ยวกับเรา</span>
+                        </a>
+                    </li>
+
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/faq">
+                        <span itemprop="title">คำถามที่พบบ่อย</span></a>
+                    </li>
+                    
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/jobs"><span itemprop="title">ร่วมงานกับเรา</span></a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-sm-6 col-xs-12">
+
+                <ul class="list-unstyled">
+
+                    <li style="display:none;" itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
+                        <a itemprop="url" href="/"><span itemprop="title">บ้าน</span></a>
+                    </li>
+
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/privacy-statement">
+                        <span itemprop="title">นโยบายสิทธิส่วนบุคคล</span></a>
+                    </li>
+
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/terms-and-conditions">
+                        <span itemprop="title">ข้อกำหนดและเงื่อนไข</span></a>
+                    </li>
+
+                    <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" itemprop="child">
+                        <a itemprop="url" target="_blank" href="/articles"><span itemprop="title">บล็อกของเรา</span></a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+            <div class="clearfix visible-xs"></div>
+
+            <div class="col-md-6 col-sm-12 col-xs-12 text-right padding-footer">
+
+                <div class="row">
+                    <div class="col-md-0">
+                        <ul class="list-unstyled list-inline mania">
+
+                            <li><div class="icon-secured-logo"></div></li>
+                            <li><div class="icon-dbd-oic-logo"></div></li>
+                            <li class="worktime">
+                                <div class="icon-24hr-phone-white pull-left"></div>
+                                <div class="number">02-022-1222</div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            <div class="row">
+
+        <address><p class="address">
+
+        <strong>Ask Hanuman Co., Ltd.</strong><br> อาคาร กริต ชั้น 2 เลขที่ 1032/1-5, 14 ถนน พระราม 4<br> แขวงทุ่งมหาเมฆ เขตสาธร กรุงเทพฯ ประเทศไทย 10120<br>
+
+        <a href="mailto:support@askhanuman.com">support@askhanuman.com</a></p></address></div>
+
+        <!-- .row --></div></div><!-- .row -->
+
+        </div><!-- .container -->
+
+
+        <div class="copyright">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-sm-12">
+
+                        <p> Copyright &copy; <time>2014</time> ASK Hanuman. All Rights Reserved. </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</footer>
+
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
-<head>
+<head <?php language_attributes(); ?> >
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
