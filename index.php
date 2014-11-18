63d52929b7c2a930d5fae7d82d5acc5f944639ec
weerapat <weerapatsu@gmail.com>
2014-11-18 10:46:05 +0700

    update facebook like and link category

--- index.php
+++ index.php
@@ -4,16 +4,22 @@
 <div class="container main-content">
 
     <div class="row">
-        <div class="col-lg-3 col-md-4">
+        <div class="col-lg-2 col-md-4">
             <?php get_sidebar(); ?>
         </div> 
 
-        <div class="col-lg-6 col-md-8">
+        <div class="col-lg-7 col-md-8">
 
             <div class="content">
             <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
               <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <div class="post-header">
+                    <div class="date"><?php the_time( 'M j y' ); ?></div>
+                    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="author"><?php the_author(); ?></div>
+                </div><!--end post header-->
 
                 <div class="entry clear">
                     <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
@@ -25,12 +31,6 @@
                     <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
                 </div><!--end post footer-->
 
-                <div class="post-header">
-                    <div class="date"><?php the_time( 'M j y' ); ?></div>
-                    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                    <div class="author"><?php the_author(); ?></div>
-                </div><!--end post header-->
-                
               </div><!--end post-->
             <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
               <div class="navigation index">
@@ -47,29 +47,27 @@
 
         <!-- Right bar widget --> 
 
-        <div class="col-md-3 visible-lg ">
+        <div class="col-md-3 visible-lg rightbar-widget">
             <div class="row">
 
                 <!-- Trustbox  --> 
-                <div class="col-md-12 trustbox">
+                <div class="col-md-12 trustbox box">
 
-                    <div class="padding-around">
-                        <ul ng-click="selectionFormShow = true" class="list-unstyled list-trust center-block font-thai">
-                            <li>
-                                <div class="trust-shield-30s pull-left"></div>
-                                <div class="trust-text">เปรียบเทียบจาก 30 แบรนด์ ภายใน 30 วินาที</div>
-                            </li>
-                            <li><div class="trust-shield-bath pull-left"></div><div class="trust-text">ยินดีคืนเงิน 100% หากเจอที่อื่นถูกกว่า</div></li>
-                            <li><div class="trust-shield-24h pull-left"></div><div class="trust-text">บริการช่วยเหลือฉุกเฉิน 24 ชม.</div></li>
-                            <li><div class="trust-shield-0per pull-left"></div><div class="trust-text">ผ่อน 0% 10 เดือน จ่ายสดลดอีก 5%</div></li>
-                            <li><div class="trust-shield-gift pull-left"></div><div class="trust-text">แจกของสมนาคุณฟรีมูลค่า4,000บาท</div></li>
-                        </ul>
-                    </div>
+                    <ul ng-click="selectionFormShow = true" class="list-unstyled list-trust center-block font-thai">
+                        <li>
+                            <div class="trust-shield-30s pull-left"></div>
+                            <div class="trust-text">เปรียบเทียบจาก 30 แบรนด์ ภายใน 30 วินาที</div>
+                        </li>
+                        <li><div class="trust-shield-bath pull-left"></div><div class="trust-text">ยินดีคืนเงิน 100% หากเจอที่อื่นถูกกว่า</div></li>
+                        <li><div class="trust-shield-24h pull-left"></div><div class="trust-text">บริการช่วยเหลือฉุกเฉิน 24 ชม.</div></li>
+                        <li><div class="trust-shield-0per pull-left"></div><div class="trust-text">ผ่อน 0% 10 เดือน จ่ายสดลดอีก 5%</div></li>
+                        <li><div class="trust-shield-gift pull-left"></div><div class="trust-text">แจกของสมนาคุณฟรีมูลค่า4,000บาท</div></li>
+                    </ul>
 
                 </div>
                 <!-- End Trustbox  --> 
 
-                <div class="col-md-12 ">
+                <div class="col-md-12 box">
 
                     <iframe id="trustedcompany-badge" src="//trustedcompany.com/embed/widget/v2?domain=askhanuman.co.th&type=b&review=1&text=a" 
                     width="100%" height="220" frameborder="0" scrolling="no">
@@ -77,7 +75,19 @@
 
                 </div>
 
+                <div class="col-md-12 box">
+                <!-- Facebook like  --> 
+                    <div class="fb-like-box" data-href="https://www.facebook.com/askhanuman" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+                </div>
 
+                <div id="fb-root"></div>
+                <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
             </div>
 
         </div>
--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,3 @@
-
-<hr class="hr-style">
-
-<div class="menu">
-
-<ul class="list-unstyled"><li ng-class="{ active: menu == 'getCar'}" class="active"><a ng-click="menu = 'getCar'" href="#">คุณจะได้รับกรมธรรม์ประกันภัยรถยนต์<br>จากอาสค์ หนุมานได้อย่างไร</a></li><li ng-class="{ active: menu == 'payPolicy'}"><hr class="hr-style"><ul class="list-unstyled"><a ng-click="menu = 'payPolicy'" href="#">วิธีการชำระค่ากรมธรรม์ประกันภัยของคุณ</a><li ng-show="menu == 'payPolicy' || menu == 'bank' || menu == 'card' || menu == 'cashOn'" style="margin-top: 12px;" class="ng-hide"><a ng-click="menu = 'bank'" href="#"><i class="fa fa-chevron-right"></i> โอนเงินผ่านธนาคาร</a></li><li ng-show="menu == 'payPolicy' || menu == 'bank' || menu == 'card' || menu == 'cashOn'" class="ng-hide"><a ng-click="menu = 'card'" href="#"><i class="fa fa-chevron-right"></i> บัตรเครดิต</a></li><li ng-show="(menu == 'payPolicy') || (menu == 'bank') || menu == 'card' || menu == 'cashOn'" class="ng-hide"><a ng-click="menu = 'cashOn'" href="#"><i class="fa fa-chevron-right"></i> ชำระเงินสดเมื่อรับกรมธรรม์</a></li></ul><hr class="hr-style"></li><li class="list-unstyled" ng-class="{ active: menu == 'sendDoc'}"><a ng-click="menu = 'sendDoc'" href="#"> เอกสารที่จำเป็น </a></li><hr class="hr-style"></ul>
-
-</div>
+<?php wp_list_categories( $args ); ?> 
 
 
-<div class="sidebar">
-<p>This is the side bar</p>
-Template files are the building blocks of your WordPress site. They fit together like the pieces of a puzzle to generate the web pages on your site. Some templates (the header and footer template files for example) are used on all the web pages, while others are used only under specific conditions.<br/><br/>
-
-A traditional web page consists of two files:<br/><br/>
-
-The XHTML page to hold the structure and content of the page and
-the CSS Style Sheet which holds the presentation styles of the page.<br/><br/>
-In WordPress, the (X)HTML structure and the CSS style sheet are present but the content is generated "behind the scenes" by various template files. The template files and the style sheet are stored together as a WordPress Theme. To learn more about creating Themes, read Theme Development.<br/><br/>
-</div>
